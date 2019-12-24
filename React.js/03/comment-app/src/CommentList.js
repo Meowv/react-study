74efe2aa383b1d6ef6bf9d45d1d40033f3e3eb89
阿星Plus <hackxing@foxmail.com>
2019-12-24 15:11:54 +0800

    实战：评论功能

---
+++ React.js/03/comment-app/src/CommentList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Comment from './Comment'
+
+class CommentList extends Component {
+    static defaultProps = {
+        comments: []
+    }
+    
+    render() {
+        return (
+            <div>
+                {this.props.comments.map((comment, index) =>
+                    <Comment comment={comment} key={index} />
+                )}
+            </div>
+        )
+    }
+}
+
+export default CommentList

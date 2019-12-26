39242035b24159b63025a12803148448bda18d1f
阿星Plus <hackxing@foxmail.com>
2019-12-26 13:33:12 +0800

    生命周期函数 component WillMount、DidMount、WillUnmount

--- React.js/02/hello-react/src/index.js
+++ React.js/02/hello-react/src/index.js
@@ -21,7 +21,26 @@ class Title extends React.Component {
 }
 
 class Header extends React.Component {
+    constructor() {
+        super()
+        console.log('constructor')
+    }
+
+    componentWillMount() {
+        console.log('component will mount')
+    }
+
+    componentDidMount() {
+        console.log('component did mount')
+    }
+
+    componentWillUnmount() {
+        console.log('component will unmount')
+    }
+
     render() {
+        console.log('render')
+
         return (
             <div>
                 <Title />
@@ -48,10 +67,24 @@ class Footer extends React.Component {
 }
 
 class Index extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isShowHeader: true
+        }
+    }
+
+    handleShowOrHide() {
+        this.setState({
+            isShowHeader: !this.state.isShowHeader
+        })
+    }
+
     render() {
         return (
             <div>
-                <Header />
+                {this.state.isShowHeader ? <Header /> : null}
+                <button onClick={this.handleShowOrHide.bind(this)}>显示或者隐藏标题</button>
                 <Main />
                 <Footer />
             </div>

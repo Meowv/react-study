7f32128ae6ead1c5e0c97650d4112fcbfd6915ad
阿星Plus <hackxing@foxmail.com>
2019-12-26 13:46:39 +0800

    ref 和 React.js 中的 DOM 操作

--- React.js/02/hello-react/src/index.js
+++ React.js/02/hello-react/src/index.js
@@ -2,58 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Clock extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            date: new Date()
-        }
-    }
-
-    componentWillMount() {
-        this.timer = setInterval(() => {
-            this.setState({ date: new Date() })
-        }, 1000)
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timer)
+class AutoFocusInput extends React.Component {
+    componentDidMount() {
+        this.input.focus()
     }
 
     render() {
         return (
-            <div>
-                <h1>
-                    <p>现在的时间是</p>
-                    {this.state.date.toLocaleTimeString()}
-                </h1>
-            </div>
+            <input ref={(input) => this.input = input} />
         )
     }
 }
 
-class Index extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            isShowHeader: true
-        }
-    }
-
-    handleShowOrHide() {
-        this.setState({
-            isShowClock: !this.state.isShowClock
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                {this.state.isShowClock ? <Clock /> : null}
-                <button onClick={this.handleShowOrHide.bind(this)}> 显示或隐藏时钟</button>
-            </div>
-        )
-    }
-}
-
-ReactDOM.render(<Index />, document.getElementById('root'));
+ReactDOM.render(<AutoFocusInput />, document.getElementById('root'));

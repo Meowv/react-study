2befa941e367dce1b3536e8191ef850c82693d39
阿星Plus <hackxing@foxmail.com>
2019-12-27 10:20:16 +0800

    抽离 store 和监控数据变化

--- React.js/make-redux/src/index.js
+++ React.js/make-redux/src/index.js
@@ -26,20 +26,54 @@ function renderContent(content) {
     contentDOM.style.color = content.color
 }
 
-function dispatch(action) {
+function createStore(state, stateChanger) {
+    const listeners = []
+    const subscribe = (listener) => listeners.push(listener)
+    const getState = () => state
+    const dispatch = (action) => {
+        stateChanger(state, action)
+        listeners.forEach((listener) => listener())
+    }
+    return { getState, dispatch, subscribe }
+}
+
+function stateChanger(state, action) {
     switch (action.type) {
         case 'UPDATE_TITLE_TEXT':
-            appState.title.text = action.text
+            state.title.text = action.text
             break
         case 'UPDATE_TITLE_COLOR':
-            appState.title.color = action.color
+            state.title.color = action.color
             break
         default:
             break
     }
 }
 
-renderApp(appState) // 首次渲染页面
-dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《哈哈哈哈哈》' }) // 修改标题文本
-dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'green' }) // 修改标题颜色
-renderApp(appState) // 把新的数据渲染到页面上
+// function dispatch(action) {
+//     switch (action.type) {
+//         case 'UPDATE_TITLE_TEXT':
+//             appState.title.text = action.text
+//             break
+//         case 'UPDATE_TITLE_COLOR':
+//             appState.title.color = action.color
+//             break
+//         default:
+//             break
+//     }
+// }
+
+const store = createStore(appState, stateChanger)
+
+store.subscribe(() => renderApp(store.getState()))
+
+// renderApp(appState) // 首次渲染页面
+// dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《哈哈哈哈哈》' }) // 修改标题文本
+// dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'green' }) // 修改标题颜色
+// renderApp(appState) // 把新的数据渲染到页面上
+
+
+renderApp(store.getState()) // 首次渲染页面
+store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《哈哈哈哈哈》' }) // 修改标题文本
+store.dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'green' }) // 修改标题颜色
+// renderApp(store.getState()) // 把新的数据渲染到页面上

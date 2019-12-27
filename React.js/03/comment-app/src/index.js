2fadaadf9b39c58e8571b42ae0b21a1768be1362
阿星Plus <hackxing@foxmail.com>
2019-12-27 20:29:28 +0800

    demo

--- React.js/03/comment-app/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CommentApp from './CommentApp'
-import './index.css';
-
-ReactDOM.render(<CommentApp />, document.getElementById('root'));

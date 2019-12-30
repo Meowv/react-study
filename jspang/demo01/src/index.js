bb077e54938125fe4c6f28cbbc7959484fa06a01
阿星Plus <hackxing@foxmail.com>
2019-12-30 10:24:07 +0800

    Fragment

---
+++ jspang/demo01/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Xiaojiejie from './Xiaojiejie'
+import './index.css';
+
+ReactDOM.render(<Xiaojiejie />, document.getElementById('root'));

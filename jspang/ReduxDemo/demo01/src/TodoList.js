812efa3adc1fccae1d0cc33f14b5c2e0645de539
阿星Plus <hackxing@foxmail.com>
2019-12-30 17:22:53 +0800

    antd、store and reducer

---
+++ jspang/ReduxDemo/demo01/src/TodoList.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import 'antd/dist/antd.css'
+import { Input, Button, List } from 'antd'
+import store from './store'
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props)
+        console.log(store.getState())
+        this.state = store.getState()
+    }
+
+    render() {
+        return (
+            <div style={{ margin: '10px' }}>
+                <div>
+                    <Input placeholder={this.state.inputValue} onChange={this} style={{ width: '250px', marginRight: '10px' }} />
+                    <Button type="primary">增加</Button>
+                </div>
+                <div style={{ margin: '10px', width: '300px' }}>
+                    <List
+                        bordered
+                        dataSource={this.state.list}
+                        renderItem={item => (<List.Item>{item}</List.Item>)}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+export default TodoList;
---
+++ jspang/ReduxDemo/demo01/src/store/reducer.js
@@ -0,0 +1,12 @@
+const defaultState = {
+    inputValue: '写点啥吧',
+    list: [
+        '早8点开晨会，分配今天的开发工作',
+        '早9点和项目经理作开发需求讨论会',
+        '晚5:30对今日代码进行review'
+    ]
+}
+
+export default (state = defaultState, action) => {
+    return state
+}

c1761cf0dab8f2f7ba43798c7003c8774787523c
阿星Plus <hackxing@foxmail.com>
2019-12-30 17:38:42 +0800

    Reduc流程

--- jspang/ReduxDemo/demo01/src/TodoList.js
+++ jspang/ReduxDemo/demo01/src/TodoList.js
@@ -1,20 +1,37 @@
-import React, { Component } from 'react'
+import { Button, Input, List } from 'antd'
 import 'antd/dist/antd.css'
-import { Input, Button, List } from 'antd'
+import React, { Component } from 'react'
 import store from './store'
 
 class TodoList extends Component {
     constructor(props) {
         super(props)
-        console.log(store.getState())
         this.state = store.getState()
+        this.changeInputValue = this.changeInputValue.bind(this)
+        this.storeChange = this.storeChange.bind(this)
+        store.subscribe(this.storeChange)
+    }
+
+    changeInputValue(e) {
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        }
+        store.dispatch(action)
+    }
+
+    storeChange() {
+        this.setState(store.getState())
     }
 
     render() {
         return (
             <div style={{ margin: '10px' }}>
                 <div>
-                    <Input placeholder={this.state.inputValue} onChange={this} style={{ width: '250px', marginRight: '10px' }} />
+                    <Input
+                        placeholder={this.state.inputValue}
+                        style={{ width: '250px', marginRight: '10px' }}
+                        onChange={this.changeInputValue} />
                     <Button type="primary">增加</Button>
                 </div>
                 <div style={{ margin: '10px', width: '300px' }}>
--- jspang/ReduxDemo/demo01/src/store/reducer.js
+++ jspang/ReduxDemo/demo01/src/store/reducer.js
@@ -8,5 +8,11 @@ const defaultState = {
 }
 
 export default (state = defaultState, action) => {
+    console.log(state, action)
+    if (action.type === 'changeInput') {
+        let newState = JSON.parse(JSON.stringify(state))
+        newState.inputValue = action.value
+        return newState
+    }
     return state
 }

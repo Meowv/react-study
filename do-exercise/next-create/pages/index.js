fc56741a57951e7ae633e0ec95c892193b37647b
阿星Plus <hackxing@foxmail.com>
2019-12-31 21:21:05 +0800

    路由，跳转时用query传递和接受参数

--- do-exercise/next-create/pages/index.js
+++ do-exercise/next-create/pages/index.js
@@ -1,18 +1,30 @@
-import React from 'react'
 import Link from 'next/link'
 import Router from 'next/router'
+import React from 'react'
 
 const Home = () => {
-    function gotoA() {
-        Router.push('/qixA')
+    function gotoXiaojiejie() {
+        // Router.push('/xiaojiejie?name=井空')
+        Router.push({
+            pathname: '/xiaojiejie',
+            query: {
+                name: '井空'
+            }
+        })
     }
     return (
         <>
             <div>我是首页</div>
-            <div><Link href="/qixA"><a>去Qix A Page</a></Link></div>
-            <div><Link href="/qixB"><a>去Qix B Page</a></Link></div>
             <div>
-                <button onClick={gotoA}>去Qix A Page</button>
+                <Link href="/xiaojiejie?name=结衣"><a>选择结衣</a></Link>
+                <br />
+                <Link href="/xiaojiejie?name=井空"><a>选择井空</a></Link>
+                <br />
+                <Link href={{ pathname: '/xiaojiejie', query: { name: '结衣' } }}><a>选择结衣</a></Link>
+            </div>
+
+            <div>
+                <button onClick={gotoXiaojiejie}>选择井空</button>
             </div>
         </>
     )

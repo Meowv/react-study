276222ac0ea1c4d835236e3c502c4df535b46512
阿星Plus <hackxing@foxmail.com>
2019-12-31 11:59:23 +0800

    代码优化，提升性能

--- do-exercise/ReduxDemo/demo02/src/TodoList.js
+++ do-exercise/ReduxDemo/demo02/src/TodoList.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
-class TodoList extends Component {
-    render() {
-        return (
+const TodoList = (props) => {
+    let { inputvalue, inputChange, clickButton, list, deleteItem } = props;
+    return (
+        <div>
             <div>
-                <div>
-                    <input value={this.props.inputValue} onChange={this.props.inputChange} />
-                    <button onClick={this.props.clickButton}>提交</button>
-                </div>
-                <ul>
-                    {
-                        this.props.list.map((item, index) => {
-                            return (
-                                <li key={index} onClick={() => this.props.deleteItem(index)}>
-                                    {item}
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
+                <input placeholder='写点啥吧' value={inputvalue} onChange={inputChange} />
+                <button onClick={clickButton}>提交</button>
             </div>
-        );
-    }
+            <ul>
+                {
+                    list.map((item, index) => {
+                        return (
+                            <li key={index} onClick={() => deleteItem(index)}>
+                                {item}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
 }
 
 const stateToProps = (state) => {

b8b59a98a6fd44c26013b3cfa98f3390523fa932
阿星Plus <hackxing@foxmail.com>
2019-12-31 14:37:46 +0800

    模拟动态获取路由进行菜单展示

--- do-exercise/ReactRouterDemo/demo02/src/AppRouter.js
+++ do-exercise/ReactRouterDemo/demo02/src/AppRouter.js
@@ -6,15 +6,27 @@ import Video from './Pages/Video';
 import WorkPlace from './Pages/WorkPlace';
 
 function AppRouter() {
+    let routerConfig = [
+        { path: '/', title: '博客首页', exact: true, component: Index },
+        { path: '/video/', title: '视频教程', exact: false, component: Video },
+        { path: '/workplace/', title: '职场技能', exact: false, component: Workplace }
+    ]
+
     return (
         <Router>
             <div className="mainDiv">
                 <div className="leftNav">
                     <h3>一级导航</h3>
                     <ul>
-                        <li><Link to="/">博客首页</Link> </li>
-                        <li><Link to="/video/">视频教程</Link> </li>
-                        <li><Link to="/workplace/">职场技能</Link> </li>
+                        {
+                            routerConfig.map((item, index) => {
+                                return (
+                                    <li key={index}>
+                                        <Link to={item.path}>{item.title}</Link>
+                                    </li>
+                                )
+                            })
+                        }
                     </ul>
                 </div>
 
@@ -22,6 +34,18 @@ function AppRouter() {
                     <Route path="/" exact component={Index} />
                     <Route path="/video/" component={Video} />
                     <Route path="/workplace/" component={WorkPlace} />
+                    {
+                        routerConfig.map((item, index) => {
+                            return (
+                                <Route
+                                    key={index}
+                                    exact={item.exact}
+                                    path={item.path}
+                                    component={item.component}
+                                />
+                            )
+                        })
+                    }
                 </div>
             </div>
         </Router>

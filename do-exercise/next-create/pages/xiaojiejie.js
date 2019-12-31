0724881e0df42e51207e04463499e44fbdb80e89
阿星Plus <hackxing@foxmail.com>
2019-12-31 21:40:26 +0800

    在getInitialProps中用axios获取远端数据

--- do-exercise/next-create/pages/xiaojiejie.js
+++ do-exercise/next-create/pages/xiaojiejie.js
@@ -1,5 +1,6 @@
 import { withRouter } from 'next/router'
 import Link from 'next/link'
+import axios from 'axios'
 
 const Xiaojiejie = ({ router }) => {
     return (
@@ -8,6 +9,18 @@ const Xiaojiejie = ({ router }) => {
             <Link href="/"><a>返回首页</a></Link>
         </>
     )
+}
+
+Xiaojiejie.getInitialProps = async () => {
+    const promise = new Promise((resolve) => {
+        axios('https://www.fastmock.site/mock/7a3e574fb43c5b6aba4be91d34925a65/react/todos').then(
+            (res) => {
+                console.log('远程数据结果：', res)
+                resolve(res.data.data)
+            }
+        )
+    })
+    return await promise
 }
 
 export default withRouter(Xiaojiejie)

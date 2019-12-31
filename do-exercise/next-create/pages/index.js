e37194bdf7b1bf72717254a1556e6ef18cefbe6a
阿星Plus <hackxing@foxmail.com>
2019-12-31 21:33:35 +0800

    路由的钩子事件

--- do-exercise/next-create/pages/index.js
+++ do-exercise/next-create/pages/index.js
@@ -3,6 +3,38 @@ import Router from 'next/router'
 import React from 'react'
 
 const Home = () => {
+
+    //routeChangeStart
+    //routeChangeComplete
+    //beforeHistoryChange
+    //routeChangeError
+    //hashChangeStart
+    //hashChangeComplete
+
+    Router.events.on('routeChangeStart', (...args) => {
+        console.log('1.routeChangeStart => 路由开始变化，参数为：', ...args)
+    })
+
+    Router.events.on('routeChangeComplete', (...args) => {
+        console.log('2.routeChangeComplete => 路由变化结束，参数为：', ...args)
+    })
+
+    Router.events.on('beforeHistoryChange', (...args) => {
+        console.log('3.beforeHistoryChange => 在改变浏览器 history 之前触发，参数为：', ...args)
+    })
+
+    Router.events.on('routeChangeError', (...args) => {
+        console.log('4.routeChangeError => 跳转发生错误，参数为：', ...args)
+    })
+
+    Router.events.on('hashChangeStart', (...args) => {
+        console.log('5.hashChangeStart => hash跳转开始时执行，参数为：', ...args)
+    })
+
+    Router.events.on('hashChangeComplete', (...args) => {
+        console.log('6.hashChangeComplete => hash跳转完成时,，参数为：', ...args)
+    })
+
     function gotoXiaojiejie() {
         // Router.push('/xiaojiejie?name=井空')
         Router.push({
@@ -25,6 +57,10 @@ const Home = () => {
 
             <div>
                 <button onClick={gotoXiaojiejie}>选择井空</button>
+            </div>
+
+            <div>
+                <Link href="#qix"><a>选qix</a></Link>
             </div>
         </>
     )

e19df96aad514ec944d8e74d238bcba779b10a22
阿星Plus <hackxing@foxmail.com>
2019-12-31 14:32:47 +0800

    完善菜单多层路由跳转

--- do-exercise/ReactRouterDemo/demo02/src/AppRouter.js
+++ do-exercise/ReactRouterDemo/demo02/src/AppRouter.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import './index.css';
 import Index from './Pages/Index';
 import Video from './Pages/Video';
+import WorkPlace from './Pages/WorkPlace';
 
 function AppRouter() {
     return (
@@ -13,13 +14,14 @@ function AppRouter() {
                     <ul>
                         <li><Link to="/">博客首页</Link> </li>
                         <li><Link to="/video/">视频教程</Link> </li>
-                        <li><Link to="">职场技能</Link> </li>
+                        <li><Link to="/workplace/">职场技能</Link> </li>
                     </ul>
                 </div>
 
                 <div className="rightMain">
                     <Route path="/" exact component={Index} />
                     <Route path="/video/" component={Video} />
+                    <Route path="/workplace/" component={WorkPlace} />
                 </div>
             </div>
         </Router>
---
+++ do-exercise/ReactRouterDemo/demo02/src/Pages/WorkPlace.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import Getup from './workPlace/Getup';
+import Money from './workPlace/Money';
+
+function WorkPlace() {
+    return (
+        <div>
+            <div className="topNav">
+                <ul>
+                    <li><Link to="/workplace/Moeny">程序员加薪秘籍</Link></li>
+                    <li><Link to="/workplace/Getup">程序员早起攻略</Link></li>
+                </ul>
+            </div>
+            <div className="videoContent">
+                <div><h3>职场软技能</h3></div>
+                <Route path="/workplace/Moeny/" component={Money} />
+                <Route path="/workplace/Getup/" component={Getup} />
+            </div>
+        </div>
+    )
+}
+
+export default WorkPlace;

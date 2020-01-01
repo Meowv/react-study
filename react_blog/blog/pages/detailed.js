9e87b6ed2aae821f56c9cb48f710575099cfa5bc
阿星Plus <hackxing@foxmail.com>
2020-01-01 21:02:47 +0800

    markdown-navbar 导航菜单

--- react_blog/blog/pages/detailed.js
+++ react_blog/blog/pages/detailed.js
@@ -1,57 +1,99 @@
 import { Breadcrumb, Col, Icon, Row } from 'antd'
 import Head from 'next/head'
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
 import Advert from '../components/Advert'
 import Author from '../components/Author'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import '../static/style/pages/detailed.css'
+import MarkNav from 'markdown-navbar';
+import 'markdown-navbar/dist/navbar.css';
 
-const Detailed = () => (
-    <>
-        <Head>
-            <title>博客详细页</title>
-        </Head>
-        <Header />
-        <Row className="comm-main" type="flex" justify="center">
-            <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}  >
-                <div>
-                    <div className="bread-div">
-                        <Breadcrumb>
-                            <Breadcrumb.Item><a href="/">首页</a></Breadcrumb.Item>
-                            <Breadcrumb.Item>视频列表</Breadcrumb.Item>
-                            <Breadcrumb.Item>xxxx</Breadcrumb.Item>
-                        </Breadcrumb>
-                    </div>
 
+const Detailed = () => {
+    let markdown = '# P01:课程介绍和环境搭建\n' +
+        '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+        '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+        '**这是加粗的文字**\n\n' +
+        '*这是倾斜的文字*`\n\n' +
+        '***这是斜体加粗的文字***\n\n' +
+        '~~这是加删除线的文字~~ \n\n' +
+        '\`console.log(111)\` \n\n' +
+        '# p02:来个Hello World 初始Vue3.0\n' +
+        '> aaaaaaaaa\n' +
+        '>> bbbbbbbbb\n' +
+        '>>> cccccccccc\n' +
+        '***\n\n\n' +
+        '# p03:Vue3.0基础知识讲解\n' +
+        '> aaaaaaaaa\n' +
+        '>> bbbbbbbbb\n' +
+        '>>> cccccccccc\n\n' +
+        '# p04:Vue3.0基础知识讲解\n' +
+        '> aaaaaaaaa\n' +
+        '>> bbbbbbbbb\n' +
+        '>>> cccccccccc\n\n' +
+        '#5 p05:Vue3.0基础知识讲解\n' +
+        '> aaaaaaaaa\n' +
+        '>> bbbbbbbbb\n' +
+        '>>> cccccccccc\n\n' +
+        '# p06:Vue3.0基础知识讲解\n' +
+        '> aaaaaaaaa\n' +
+        '>> bbbbbbbbb\n' +
+        '>>> cccccccccc\n\n' +
+        '# p07:Vue3.0基础知识讲解\n' +
+        '> aaaaaaaaa\n' +
+        '>> bbbbbbbbb\n' +
+        '>>> cccccccccc\n\n' +
+        '``` var a=11; ```'
+
+    return (
+        <>
+            <Head>
+                <title>博客详细页</title>
+            </Head>
+            <Header />
+            <Row className="comm-main" type="flex" justify="center">
+                <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}  >
                     <div>
-                        <div className="detailed-title">
-                            50元加入小密圈 胖哥带你学一年
+                        <div className="bread-div">
+                            <Breadcrumb>
+                                <Breadcrumb.Item><a href="/">首页</a></Breadcrumb.Item>
+                                <Breadcrumb.Item>视频列表</Breadcrumb.Item>
+                                <Breadcrumb.Item>xxxx</Breadcrumb.Item>
+                            </Breadcrumb>
                         </div>
-
-                        <div className="list-icon center">
-                            <span><Icon type="calendar" /> 2020-01-01</span>
-                            <span><Icon type="folder" /> 视频教程</span>
-                            <span><Icon type="fire" /> 5498人</span>
+                        <div>
+                            <div className="detailed-title">
+                                50元加入小密圈 胖哥带你学一年
+                            </div>
+                            <div className="list-icon center">
+                                <span><Icon type="calendar" /> 2020-01-01</span>
+                                <span><Icon type="folder" /> 视频教程</span>
+                                <span><Icon type="fire" /> 5498人</span>
+                            </div>
+                            <div className="detailed-content" >
+                                <ReactMarkdown source={markdown} escapeHtml={false} />
+                            </div>
                         </div>
-
-                        <div className="detailed-content" >
-                            详细内容
-                        </div>
-
                     </div>
-
-                </div>
-            </Col>
-
-            <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
-                <Author />
-                <Advert />
-
-            </Col>
-        </Row>
-        <Footer />
-    </>
-)
+                </Col>
+                <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
+                    <Author />
+                    <Advert />
+                    <div className="detailed-nav comm-box">
+                        <div className="nav-title">文章目录</div>
+                        <MarkNav
+                            className="article-menu"
+                            source={markdown}
+                            ordered={false}
+                        />
+                    </div>
+                </Col>
+            </Row>
+            <Footer />
+        </>
+    )
+}
 
 export default Detailed

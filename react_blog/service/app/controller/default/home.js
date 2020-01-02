d59a45f303e3e2d63d6a87825e5834ba57eac525
阿星Plus <hackxing@foxmail.com>
2020-01-02 20:23:27 +0800

    文章列表接口

--- react_blog/service/app/controller/default/home.js
+++ react_blog/service/app/controller/default/home.js
@@ -9,6 +9,20 @@ class HomeController extends Controller {
     console.log(result);
     this.ctx.body = result;
   }
+
+  async getArticleList() {
+    const sql = 'SELECT article.id as id,' +
+      'article.title as title,' +
+      'article.introduce as introduce,' +
+      'article.addTime as addTime,' +
+      'article.view_count as view_count ,' +
+      'type.typeName as typeName ' +
+      'FROM article LEFT JOIN type ON article.type_id = type.Id';
+    const results = await this.app.mysql.query(sql);
+    this.ctx.body = {
+      data: results,
+    };
+  }
 }
 
 module.exports = HomeController;

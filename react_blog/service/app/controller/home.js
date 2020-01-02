cab47f32ea4694b12a60f795a567539d40782417
阿星Plus <hackxing@foxmail.com>
2020-01-02 19:47:05 +0800

    在egg.js中的路由配置

--- react_blog/service/app/controller/home.js
+++ react_blog/service/app/controller/home.js
@@ -7,6 +7,11 @@ class HomeController extends Controller {
     const { ctx } = this;
     ctx.body = 'hi, egg';
   }
+
+  async list() {
+    const { ctx } = this;
+    ctx.body = '<h1>qix</h1>';
+  }
 }
 
 module.exports = HomeController;

4616c58c297a4f88e8e42c8b779c07ee98aa4db8
阿星Plus <hackxing@foxmail.com>
2020-01-02 16:14:43 +0800

    koa 读取Cookie

--- koa2/demo7.js
+++ koa2/demo7.js
@@ -4,10 +4,21 @@ const app = new Koa();
 
 app.use(async (ctx) => {
     if (ctx.url === '/index') {
-        ctx.cookies.set('name', 'qix');
+        ctx.cookies.set('name', 'qix', {
+            domain: '127.0.0.1', // 写cookie所在的域名
+            path: '/index',       // 写cookie所在的路径
+            maxAge: 1000 * 60 * 60 * 24,   // cookie有效时长
+            expires: new Date('2020-12-31'), // cookie失效时间
+            httpOnly: false,  // 是否只用于http请求中获取
+            overwrite: false  // 是否允许重写
+        });
         ctx.body = 'cookie is ok';
     } else {
-        ctx.body = 'hello';
+        if (ctx.cookies.get('name')) {
+            ctx.body = ctx.cookies.get('name');
+        } else {
+            ctx.body = 'Cookie is none';
+        }
     }
 });
 

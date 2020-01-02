c6715560f21d72b4af2dc54a506e06c50c987121
阿星Plus <hackxing@foxmail.com>
2020-01-02 13:08:55 +0800

    解析post 参数

--- koa2/demo2.js
+++ koa2/demo2.js
@@ -18,12 +18,42 @@ app.use(async (ctx) => {
         `;
         ctx.body = html;
     } else if (ctx.url === '/' && ctx.method === 'POST') {
-        ctx.body = '接收到请求';
+        let postData = await parsePostData(ctx);
+        ctx.body = postData;
     } else {
         //其它请求显示404页面
         ctx.body = '<h1>404!</h1>';
     }
 })
+
+function parsePostData(ctx) {
+    return new Promise((resolve, reject) => {
+        try {
+            let postdata = '';
+            ctx.req.on('data', (data) => {
+                postdata += data;
+            })
+            ctx.req.addListener('end', function () {
+                let parsrData = parseQueryStr(postdata)
+                resolve(parsrData);
+            })
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+function parseQueryStr(queryStr) {
+    let queryData = {};
+    let queryStrList = queryStr.split('&');
+    console.log(queryStrList);
+    for (let [index, queryStr] of queryStrList.entries()) {
+        let itemList = queryStr.split('=');
+        console.log(itemList);
+        queryData[itemList[0]] = decodeURIComponent(itemList[1]);
+    }
+    return queryData;
+}
 
 app.listen(3000, () => {
     console.log('[demo] server is starting at port 3000');

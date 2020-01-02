1e7d4987ec7ec9df92eeb8143735fd8b1ea0153b
阿星Plus <hackxing@foxmail.com>
2020-01-02 20:43:03 +0800

    前台读取首页文章列表接口，进行展示

--- react_blog/service/app/controller/default/home.js
+++ react_blog/service/app/controller/default/home.js
@@ -14,7 +14,7 @@ class HomeController extends Controller {
     const sql = 'SELECT article.id as id,' +
       'article.title as title,' +
       'article.introduce as introduce,' +
-      'article.addTime as addTime,' +
+      "FROM_UNIXTIME(article.addTime,'%Y-%m-%d %H:%i:%s' ) as addTime," +
       'article.view_count as view_count ,' +
       'type.typeName as typeName ' +
       'FROM article LEFT JOIN type ON article.type_id = type.Id';

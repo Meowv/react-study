c43781e025993e6edacc0221591682867edf8452
阿星Plus <hackxing@foxmail.com>
2020-01-03 21:27:22 +0800

    后台，登录界面

---
+++ react_blog/admin/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+
+function Login() {
+  return (
+    <div className="App">
+      <Button type="primary">Hello World</Button>
+    </div>
+  );
+}
+
+export default Login;

24f5c0b5e626637122f674011aff14f886740d43
阿星Plus <hackxing@foxmail.com>
2020-01-03 22:08:01 +0800

    添加文章实时预览动态交互功能

--- react_blog/admin/src/pages/AddArticle.js
+++ react_blog/admin/src/pages/AddArticle.js
@@ -7,6 +7,40 @@ const { Option } = Select;
 const { TextArea } = Input;
 
 function AddArticle() {
+    const [articleId, setArticleId] = useState(0)  // 文章的ID，如果是0说明是新增加，如果不是0，说明是修改
+    const [articleTitle, setArticleTitle] = useState('')   //文章标题
+    const [articleContent, setArticleContent] = useState('')  //markdown的编辑内容
+    const [markdownContent, setMarkdownContent] = useState('预览内容') //html内容
+    const [introducemd, setIntroducemd] = useState()            //简介的markdown内容
+    const [introducehtml, setIntroducehtml] = useState('等待编辑') //简介的html内容
+    const [showDate, setShowDate] = useState()   //发布日期
+    const [updateDate, setUpdateDate] = useState() //修改日志的日期
+    const [typeInfo, setTypeInfo] = useState([]) // 文章类别信息
+    const [selectedType, setSelectType] = useState(1) //选择的文章类别
+
+    marked.setOptions({
+        renderer: new marked.Renderer(),
+        gfm: true,
+        pedantic: false,
+        sanitize: false,
+        tables: true,
+        breaks: false,
+        smartLists: true,
+        smartypants: false,
+    });
+
+    const changeContent = (e) => {
+        setArticleContent(e.target.value)
+        let html = marked(e.target.value)
+        setMarkdownContent(html)
+    }
+
+    const changeIntroduce = (e) => {
+        setIntroducemd(e.target.value)
+        let html = marked(e.target.value)
+        setIntroducehtml(html)
+    }
+
     return (
         <div>
             <Row gutter={5}>
@@ -19,7 +53,7 @@ function AddArticle() {
                         </Col>
                         <Col span={4}>
                             &nbsp;
-                    <Select defaultValue="Sign Up" size="large">
+                            <Select defaultValue="Sign Up" size="large">
                                 <Option value="Sign Up">视频教程</Option>
                             </Select>
                         </Col>
@@ -30,22 +64,51 @@ function AddArticle() {
                             <TextArea
                                 className="markdown-content"
                                 rows={35}
+                                value={articleContent}
+                                onChange={changeContent}
+                                onPressEnter={changeContent}
                                 placeholder="文章内容"
                             />
                         </Col>
                         <Col span={12}>
                             <div
-                                className="show-html">
-
+                                className="show-html"
+                                dangerouslySetInnerHTML={{ __html: markdownContent }} >
                             </div>
-
                         </Col>
                     </Row>
                 </Col>
 
                 <Col span={6}>
-
-
+                    <Row>
+                        <Col span={24}>
+                            <Button size="large">暂存文章</Button>&nbsp;
+                            <Button type="primary" size="large">发布文章</Button>
+                            <br />
+                        </Col>
+                    </Row>
+                    <Col span={24}>
+                        <br />
+                        <TextArea
+                            rows={4}
+                            value={introducemd}
+                            onChange={changeIntroduce}
+                            onPressEnter={changeIntroduce}
+                            placeholder="文章简介"
+                        />
+                        <br /><br />
+                        <div className="introduce-html"
+                            dangerouslySetInnerHTML={{ __html: '文章简介：' + introducehtml }}>
+                        </div>
+                    </Col>
+                    <Col span={12}>
+                        <div className="date-select">
+                            <DatePicker
+                                placeholder="发布日期"
+                                size="large"
+                            />
+                        </div>
+                    </Col>
                 </Col>
             </Row>
         </div>

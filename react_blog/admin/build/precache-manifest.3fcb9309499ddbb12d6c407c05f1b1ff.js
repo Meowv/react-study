7408a50645ca4ffd315ef0bb5638f52d7c2e5dad
阿星Plus <hackxing@foxmail.com>
2020-01-05 18:09:55 +0800

    npm run build / yarn build

---
+++ react_blog/admin/build/precache-manifest.3fcb9309499ddbb12d6c407c05f1b1ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95076945a253011b08bf2c4ba84db9a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "764f63b5b60da646b8ba",
+    "url": "/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "26fb9b8648afa447bdda",
+    "url": "/static/css/main.6cce3950.chunk.css"
+  },
+  {
+    "revision": "764f63b5b60da646b8ba",
+    "url": "/static/js/2.98ef09f6.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/static/js/2.98ef09f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26fb9b8648afa447bdda",
+    "url": "/static/js/main.6bdc2b4c.chunk.js"
+  },
+  {
+    "revision": "ead00dad6c75563c7a7e",
+    "url": "/static/js/runtime-main.79802dc9.js"
+  }
+]);

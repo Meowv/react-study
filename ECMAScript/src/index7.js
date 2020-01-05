f1ac89a9c43abe1e372e6cb80594e34c6cd81024
阿星Plus <hackxing@foxmail.com>
2020-01-05 21:29:51 +0800

    函数和数组相关操作技巧

--- ECMAScript/src/index7.js
+++ ECMAScript/src/index7.js
@@ -40,3 +40,47 @@ var add5 = (a, b = 1) => {
     return a + b;
 }
 console.log(add5(1))
+
+let json = {
+    a: 'qix',
+    b: 'aaa'
+}
+function func({ a, b = 'qix' }) {
+    console.log(a, b)
+}
+func(json)
+
+console.log('b' in json); // true
+
+let arr6 = ['qix', 'meowv', 'aaa'];
+function func2(a, b, c) {
+    console.log(a, b, c)
+}
+func2(...arr6)
+
+console.log(0 in arr6); // true
+
+let arr7 = [, , , , ,];
+console.log(arr7.length); // 5
+console.log(0 in arr7); // false
+
+// 数组的遍历方法
+let arr8 = ['www', 'meowv', 'com'];
+
+// forEach
+arr8.forEach((val, index) => console.log(index, val));
+
+// filter
+arr8.filter(x => console.log(x))
+
+// some
+arr8.some(x => console.log(x))
+
+// map 替换
+console.log(arr8.map(x => 'web'))
+
+// join()
+console.log(arr8.join('.'))
+
+// toString*()
+console.log(arr8.toString());

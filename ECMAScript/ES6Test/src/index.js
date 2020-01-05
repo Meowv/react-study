fb8c77b598eb9e8198e54412f67e226f527f5796
阿星Plus <hackxing@foxmail.com>
2020-01-05 19:49:19 +0800

    let const var

--- ECMAScript/ES6Test/src/index.js
+++ ECMAScript/ES6Test/src/index.js
@@ -8,3 +8,37 @@ console.log(a);
  * babel src / index.js - o dist / index.js
  * npm run build
  */
+
+/**
+ * var：它是variable的简写，可以理解成变量的意思。
+ * let：它在英文中是“让”的意思，也可以理解为一种声明的意思。
+ * var：它在英文中也是常量的意思，在ES6也是用来声明常量的，常量你可以简单理解为不变的量。
+ */
+
+var aa = 'qix';
+console.log(aa);
+
+window.onload = function () {
+    console.log(aa);
+};
+
+{
+    // var aa = '阿星Plus';
+    let aa = '阿星Plus';
+}
+console.log(aa);
+
+for (var i = 0; i < 10; i++) {
+    console.log('循环体中:' + i);
+}
+console.log('循环体外:' + i);
+
+for (let i2 = 0; i2 < 10; i2++) {
+    console.log('循环体中:' + i2);
+}
+
+// console.log('循环体外:' + i2); Uncaught ReferenceError: i2 is not defined
+
+const x = 'qix';
+// var x = '阿星Plus'; const声明的变量是不可以改变的
+console.log(x);

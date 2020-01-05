f28dea3b9892debfb06e713b111432664c5e2df5
阿星Plus <hackxing@foxmail.com>
2020-01-05 19:36:43 +0800

    ES6开发环境搭建，babel 转换 js

---
+++ ECMAScript/ES6Test/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var a = 1;
+console.log(a);
+
+/**
+ * npm init -y
+ * npm install -g babel-preset-es2015 babel-cli
+ * npm install --save babel-preset-es2015 babel-cli
+ * babel src / index.js - o dist / index.js
+ */
---
+++ ECMAScript/ES6Test/src/index.js
@@ -0,0 +1,10 @@
+let a = 1;
+console.log(a);
+
+/**
+ * npm init -y
+ * npm install -g babel-preset-es2015 babel-cli
+ * npm install --save babel-preset-es2015 babel-cli
+ * babel src / index.js - o dist / index.js
+ * npm run build
+ */

b521dedf9f72a2c3e0960ac16bddadb4b1f73732
阿星Plus <hackxing@foxmail.com>
2020-01-07 19:54:22 +0800

    模块化操作

---
+++ ECMAScript/src/index15.js
@@ -0,0 +1,6 @@
+// export : 负责进行模块化，也是模块的输出。
+// import : 负责把模块引，也是模块的引入操作。
+
+import { name, cname, skill } from './temp';
+
+console.log(name);
---
+++ ECMAScript/src/temp.js
@@ -0,0 +1,17 @@
+var a = 'qix';
+var b = '阿星Plus';
+var c = 'web';
+
+// export { a, b, c };
+
+// export function add(a, b) {
+//     return a + b;
+// }
+
+export {
+    name as a,
+    cname as b,
+    skill as c,
+}
+
+// export default var aa = 'qix';

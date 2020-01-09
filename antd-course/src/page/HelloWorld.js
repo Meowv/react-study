b9ff0238496ec9e0b22044d8943c72d26ef26946
阿星Plus <hackxing@foxmail.com>
2020-01-09 14:32:52 +0800

    antd Card组件

--- antd-course/src/page/HelloWorld.js
+++ antd-course/src/page/HelloWorld.js
@@ -1,3 +1,24 @@
+import { Card } from 'antd';
+
 export default () => {
-    return <div>hello world</div>;
+    const style = {
+        width: '400px',
+        margin: '30px',
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
+        border: '1px solid #e8e8e8',
+    };
+
+    return (
+        <Card style={style} actions={[<a>操作一</a>, <a>操作二</a>]}>
+            <Card.Meta
+                avatar={<img
+                    alt=""
+                    style={{ width: '64px', height: '64px', borderRadius: '32px' }}
+                    src="https://static.meowv.com/images/avatar.jpg"
+                />}
+                title="阿星Plus"
+                description="生命不息，奋斗不止"
+            />
+        </Card>
+    );
 }
